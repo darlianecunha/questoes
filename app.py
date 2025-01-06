b261f142c5d62b6800343d2ad9d50d8bd887c08d
Darliane <140345464+darlianecunha@users.noreply.github.com>
2025-01-06 21:52:37 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ st.markdown("<h1 style='color: darkblue;'>Relationship Questions</h1>", unsafe_a
 scores = []
 categories = []
 
-st.write("Avalie as seguintes questões em uma escala de 1 a 10 (1: Discordo totalmente, 10: Concordo totalmente)")
+st.write("Please rate the following questions on a scale of 1 to 10 (1: Strongly disagree, 10: Strongly agree")
 for i, question in enumerate(questions):
     score = st.slider(f"{i+1}. {question}", 1, 10, 5)
     scores.append(score)
